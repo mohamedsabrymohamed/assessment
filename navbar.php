bf0e5a5a95f805a437ef1ad7460b02b2db8ef1e9
mohamed sabry <mohamed.sabry@mail.com>
2020-06-19 10:00:01 +0200

    complete navbar

---
+++ navbar.php
@@ -0,0 +1,8 @@
+<nav class="navtop">
+    <div>
+        <h1>Assessment</h1>
+        <a href="profile.php"><i class="fas fa-user-circle"></i>Profile</a>
+        <a href="add_case.php"><i class="fa fa-calendar"></i>Add Case</a>
+        <a href="logout.php"><i class="fas fa-sign-out-alt"></i>Logout</a>
+    </div>
+</nav>
